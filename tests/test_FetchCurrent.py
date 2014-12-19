5949cbb4dba80e67bc5587a6695c8b96c959fade
Adam Gilman <adam.gilman@gmail.com>
2014-12-19 20:03:32 +0000

    clean up dead tests

--- tests/test_FetchCurrent.py
+++ tests/test_FetchCurrent.py
@@ -16,46 +16,6 @@ logging.basicConfig() # you need to initialize logging, otherwise you will not s
 vcr_log = logging.getLogger("vcr")
 vcr_log.setLevel(logging.DEBUG)
 
-
-'''
-	def test_InvalidStationOnLine(self):
-		self.assertIsNone(self.status.getStatus("D", "OXC"))
-		self.assertIsNotNone(self.status.getStatus("V", "OXC"))
-		self.assertIsNotNone(self.status.getStatus("B", "OXC"))
-		self.assertIsNotNone(self.status.getStatus("C", "OXC"))
-
-	def test_NumberPlatformsAtStation(self):
-		v_bhr = self.status.getStatus("V", "BHR")
-		self.assertEqual(len(v_bhr.platforms), 2)
-
-	def test_NextTrainsOnPlatform(self):
-		print self.status.getStatus("V", "OXC")
-		self.assertTrue(False)
-'''
-
-class TestTFLFetchCurrent(unittest.TestCase):
-	def setUp(self):
-		self.tfl = TFL()
-
-	def test_GetPlatformInvalidStationOnLine(self):
-		#self.assertIsNone( self.tfl.getPlatforms(self.tfl.map.stations['OXC'], self.tfl.map.lines['D']) )
-		#self.assertIsNotNone( self.tfl.getPlatforms(self.tfl.map.stations['OXC'], self.tfl.map.lines['C']) )
-		pass
-
-	def test_GetPlatformsOnStationLine(self):
-		#platforms = self.tfl.getPlatforms(self.tfl.map.stations['OXC'], self.tfl.map.lines['B'])
-		#self.assertIsInstance(platforms, dict)
-		#self.assertIsInstance(platforms.iteritems().next(), TFLPlatform)
-		pass
-		#self.assertIsInstance(platforms[0].name, unicode)
-		#self.assertTrue( len(platforms[0].name) is not 0 )
-	
-#	def test_GetNextTrainsOnPlatform(self):
-#		platforms = self.tfl.getPlatforms(self.tfl.map.stations['OXC'], self.tfl.map.lines['B'])
-#		self.assertIsInstance( platforms[0].trains, list)
-
-		#self.assertIsInstance( platforms[0].trains[0], TFLTrain)
-
 class TestTFLPlatforms(unittest.TestCase):
 	def setUp(self):
 		self.tfl = TFL()
@@ -69,7 +29,6 @@ class TestTFLPlatforms(unittest.TestCase):
 			first_platform = platforms[platforms.keys()[0]]
 			self.assertIsInstance( first_platform, TFLPlatform)
 			self.assertEqual( first_platform.name, "Northbound - Platform 4")
-		#pass
 
 class TestTFLTubeMap(unittest.TestCase):
 	def setUp(self):
@@ -103,6 +62,3 @@ class TestTFLTubeMap(unittest.TestCase):
 	def test_FailGetStationLine(self):
 		#district line does not go through OXC
 		self.assertIsNone(self.tfl.map.get(linecode="D", stationcode="OXC"))
-
-
-	#self.assertEqual(len(self.current.stations), 263)
