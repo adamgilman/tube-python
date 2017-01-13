3d94e79f7d367eed95ed68b53d0ab13a36cc3219
AdamG <adam.gilman@gmail.com>
2017-01-13 17:02:07 +0000

    bump pypi version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='tube',
-    version='1.2',
+    version='1.2.1',
     author='Adam Gilman',
     author_email='me@adamgilman.com',
     packages=['tube'],
